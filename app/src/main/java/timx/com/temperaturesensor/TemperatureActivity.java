636316e45663f929e2d5a6b0385b0e9a22594af9
sweetim <hosweetim@gmail.com>
2015-01-13 13:34:03 +0900

    added graph library

--- app/src/main/java/timx/com/temperaturesensor/TemperatureActivity.java
+++ app/src/main/java/timx/com/temperaturesensor/TemperatureActivity.java
@@ -3,6 +3,7 @@ package timx.com.temperaturesensor;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
@@ -13,6 +14,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
 import timx.com.temperaturesensor.Connectivity.Bluetooth.BTServiceConstant;
 import timx.com.temperaturesensor.Connectivity.Bluetooth.BluetoothService;
 
@@ -22,6 +30,8 @@ public class TemperatureActivity extends ActionBarActivity {
 
     private BluetoothAdapter mBTAdapter = null;
     private BluetoothService mBTService = null;
+
+    private LineChart chart_temperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +51,61 @@ public class TemperatureActivity extends ActionBarActivity {
             setSupportActionBar(main_toolbar);
             getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
         }
+
+        chart_temperature = (LineChart) findViewById(R.id.chart_temperature);
+        chart_temperature.setStartAtZero(true);
+        chart_temperature.setDrawYValues(false);
+        chart_temperature.setDrawXLabels(false);
+        chart_temperature.setDrawYLabels(false);
+        chart_temperature.setDrawHorizontalGrid(false);
+        chart_temperature.setDrawBorder(false);
+        chart_temperature.setDrawLegend(false);
+        chart_temperature.setDragEnabled(true);
+        chart_temperature.setScaleEnabled(true);
+        chart_temperature.setPinchZoom(false);
+        chart_temperature.setDrawGridBackground(false);
+        chart_temperature.setDrawVerticalGrid(false);
+
+        setData(10, 10);
+
     }
+
+    private void setData(int count, float range) {
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            xVals.add(Integer.toString(i));
+        }
+
+        ArrayList<Entry> vals1 = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+            float val = (float) (Math.random() * 10);
+            vals1.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(vals1, "DataSet 1");
+        set1.setDrawCubic(true);
+        set1.setCubicIntensity(0.2f);
+        set1.setDrawFilled(true);
+        set1.setDrawCircles(false);
+        set1.setLineWidth(2f);
+        set1.setCircleSize(5f);
+        set1.setHighLightColor(Color.rgb(244, 117, 117));
+        set1.setColor(Color.rgb(104, 241, 175));
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(set1);
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, dataSets);
+
+        // set data
+        chart_temperature.setData(data);
+        chart_temperature.invalidate();
+    }
+
 
     @Override
     protected void onStart() {
